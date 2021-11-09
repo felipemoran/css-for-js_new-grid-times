d7b18370d84d66b0c1f5cdea8443bae4a12e4630
Felipe Morán <felipe.moran11@gmail.com>
2021-11-09 23:02:37 +0000

    Implements secondary story layout for tablet

--- src/components/SecondaryStory/SecondaryStory.js
+++ src/components/SecondaryStory/SecondaryStory.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import {QUERIES} from "../../constants";
 
 const SecondaryStory = ({id, title, image, location, abstract}) => {
     return (
@@ -21,6 +22,16 @@ const Wrapper = styled.article`
   gap: 4px 16px;
   grid-template-columns: 120px 1fr;
   color: var(--color-gray-900);
+  align-items: start;
+  
+  @media ${QUERIES.tabletOnly} {
+    grid-template-columns: 1fr;
+    grid-template-areas:
+      'image'
+      'heading'
+      'abstract';
+    gap: 4px 0;
+  }
 `;
 
 const Image = styled.img`
@@ -45,6 +56,13 @@ const Abstract = styled.p`
   grid-area: abstract;
   font-size: 1rem;
   white-space: pre-wrap;
+  
+  // necessary for line-clamping
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  text-overflow: ellipsis;
 `;
 
 export default SecondaryStory;
--- src/components/SectionTitle/SectionTitle.js
+++ src/components/SectionTitle/SectionTitle.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import styled from 'styled-components/macro';
+import {QUERIES} from "../../constants";
 
 const SectionTitle = ({children, cornerLink}) => {
     return (
@@ -19,6 +20,10 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+  
+  @media ${QUERIES.tabletOnly} {
+    padding-left: 2px;
+  }
 `;
 
 const Title = styled.h2`
